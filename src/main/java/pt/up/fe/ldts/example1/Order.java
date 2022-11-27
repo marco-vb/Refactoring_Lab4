0ddba85654a9c9224ac0ba5897525a259069e749
marco-vb <marcofilipe03@hotmail.com>
2022-11-27 20:19:25 +0000

    First example done

--- src/main/java/pt/up/fe/ldts/example1/Order.java
+++ src/main/java/pt/up/fe/ldts/example1/Order.java
@@ -17,7 +17,7 @@ public class Order {
     private double getTotal() {
         double total = 0;
         for (OrderLine line : lines)
-            total += line.getProduct().getPrice() * line.getQuantity();
+            total += line.getTotal();
         return total;
     }
 
@@ -28,10 +28,7 @@ public class Order {
     public String printOrder() {
         StringBuffer printBuffer = new StringBuffer();
         for (OrderLine line : lines)
-            printBuffer.append(line.getProduct().getName() +
-                    "(x" + line.getQuantity() + "): " +
-                    (line.getProduct().getPrice() * line.getQuantity()) +
-                    "\n");
+            printBuffer.append(line.toString());
         printBuffer.append("Total: " + getTotal());
         return printBuffer.toString();
     }
